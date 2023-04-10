70558dcfeef6128e9225bcbff8fd2ad8fcc2aee9
Juan <im.jan.caser@gmail.com>
2023-04-10 17:22:03 +0800

    Remove referenced code and comment

--- test/drone-controler.py
+++ test/drone-controler.py
@@ -2,37 +2,59 @@ from lib.drone.ryze_tello import Drone
 import pygame
 import numpy as np
 import time
+import cv2
+import sys
 
-# Speed of the drone
-# 无人机的速度
-S = 60
-# Frames per second of the pygame window display
-# A low number also results in input lag, as input information is processed once per frame.
-# pygame窗口显示的帧数
-# 较低的帧数会导致输入延迟，因为一帧只会处理一次输入信息
-FPS = 120
+DRONE_SPEED = 60
 
 
 class DroneController(object):
-    """ Maintains the Tello display and moves it through the keyboard keys.
-        Press escape key to quit.
-        The controls are:
-            - T: Takeoff
-            - L: Land
-            - Arrow keys: Forward, backward, left and right.
-            - A and D: Counter clockwise and clockwise rotations (yaw)
-            - W and S: Up and down.
+    # +--------------------------------------------------------------+
+    # Keymaps
+    # +--------------------------------------------------------------+
+    KEY_QUIT = pygame.K_q
+    KEY_TAKEOFF = pygame.K_t
+    KEY_LANDING = pygame.K_l
 
-        保持Tello画面显示并用键盘移动它
-        按下ESC键退出
-        操作说明：
-            T：起飞
-            L：降落
-            方向键：前后左右
-            A和D：逆时针与顺时针转向
-            W和S：上升与下降
+    # Mode 1
+    MODE1_KEY_THROTTLE_UP = pygame.K_UP  # Ascend
+    MODE1_KEY_THROTTLE_DOWN = pygame.K_DOWN  # Descend
+    MODE1_KEY_ROLL_LEFT = pygame.K_LEFT  # Move Left
+    MODE1_KEY_ROLL_RIGHT = pygame.K_RIGHT  # Move Right
+    MODE1_KEY_PITCH_UP = pygame.K_s  # Move backward
+    MODE1_KEY_PITCH_DOWN = pygame.K_w  # Move forward
+    MODE1_KEY_YAW_LEFT = pygame.K_a  # Rotate ccw
+    MODE1_KEY_YAW_RIGHT = pygame.K_d  # Rotate cw
 
-    """
+    # Mode 2
+    MODE2_KEY_THROTTLE_UP = pygame.K_w  # Ascend
+    MODE2_KEY_THROTTLE_DOWN = pygame.K_s  # Descend
+    MODE2_KEY_ROLL_LEFT = pygame.K_LEFT  # Move Left
+    MODE2_KEY_ROLL_RIGHT = pygame.K_RIGHT  # Move Right
+    MODE2_KEY_PITCH_UP = pygame.K_UP  # Move backward
+    MODE2_KEY_PITCH_DOWN = pygame.K_DOWN  # Move forward
+    MODE2_KEY_YAW_LEFT = pygame.K_a  # Rotate ccw
+    MODE2_KEY_YAW_RIGHT = pygame.K_d  # Rotate cw
+
+    # Mode 3
+    MODE3_KEY_THROTTLE_UP = pygame.K_UP  # Ascend
+    MODE3_KEY_THROTTLE_DOWN = pygame.K_DOWN  # Descend
+    MODE3_KEY_ROLL_LEFT = pygame.K_a  # Move Left
+    MODE3_KEY_ROLL_RIGHT = pygame.K_d  # Move Right
+    MODE3_KEY_PITCH_UP = pygame.K_w  # Move backward
+    MODE3_KEY_PITCH_DOWN = pygame.K_s  # Move forward
+    MODE3_KEY_YAW_LEFT = pygame.K_LEFT  # Rotate ccw
+    MODE3_KEY_YAW_RIGHT = pygame.K_RIGHT  # Rotate cw
+
+    # Mode 4
+    MODE4_KEY_THROTTLE_UP = pygame.K_w  # Ascend
+    MODE4_KEY_THROTTLE_DOWN = pygame.K_s  # Descend
+    MODE4_KEY_ROLL_LEFT = pygame.K_a  # Move Left
+    MODE4_KEY_ROLL_RIGHT = pygame.K_d  # Move Right
+    MODE4_KEY_PITCH_UP = pygame.K_UP  # Move backward
+    MODE4_KEY_PITCH_DOWN = pygame.K_DOWN  # Move forward
+    MODE4_KEY_YAW_LEFT = pygame.K_LEFT  # Rotate ccw
+    MODE4_KEY_YAW_RIGHT = pygame.K_RIGHT  # Rotate cw
 
     # +--------------------------------------------------------------+
     # PyGame variables
@@ -46,10 +68,10 @@ class DroneController(object):
     # Drone object variables
     # +--------------------------------------------------------------+
     DRONE = None  # Drone object instance
-    DRONE_SPEED = 60
-    DRONE_SEND_RC_CONTROL = False
+    DRONE_SEND_RC_COMMAND = False
 
     # Drone velocities between -100~100
+    VELOCITY_FORWARD_BACK = 0
     VELOCITY_LEFT_RIGHT = 0
     VELOCITY_UP_DOWN = 0
     VELOCITY_YAW = 0
@@ -58,7 +80,8 @@ class DroneController(object):
         pygame.init()
         pygame.display.set_caption("Video Stream")
         if self.DISPLAY_MODE == 'fullscreen':
-            self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+            self.screen = pygame.display.set_mode([0, 0],
+                                                  pygame.FULLSCREEN)
         else:
             self.screen = pygame.display.set_mode(self.DISPLAY_MODE)
 
@@ -68,6 +91,16 @@ class DroneController(object):
         # create update timer
         pygame.time.set_timer(pygame.USEREVENT + 1, 1000 // self.DISPLAY_FPS)
 
+        # Set keymap, update what suits your need, standard default
+        # on most drone is mode 2
+        self.KEY_UP, self.KEY_DOWN, \
+            self.KEY_LEFT, self.KEY_RIGHT, \
+            self.KEY_BACKWARD, self.KEY_FORWARD, \
+            self.KEY_ROTATE_LEFT, self.KEY_ROTATE_RIGHT = (self.MODE2_KEY_THROTTLE_UP, self.MODE2_KEY_THROTTLE_DOWN,
+                                                           self.MODE2_KEY_ROLL_LEFT, self.MODE2_KEY_ROLL_RIGHT,
+                                                           self.MODE2_KEY_PITCH_DOWN, self.MODE2_KEY_PITCH_UP,
+                                                           self.MODE2_KEY_YAW_LEFT, self.MODE2_KEY_YAW_RIGHT)
+
     def run(self):
 
         # Initiate connection to drone object
@@ -75,19 +108,21 @@ class DroneController(object):
         # Start video streaming
         self.DRONE.start_video_streaming()
 
-
-        frame_read = self.tello.get_frame_read()
-
+        frame_read = self.DRONE.get_video_frames()
         should_stop = False
         while not should_stop:
 
+            if self.DRONE.is_low_battery is True:
+                # Gracefull exit
+                self.DRONE.bye()
             for event in pygame.event.get():
                 if event.type == pygame.USEREVENT + 1:
-                    self.update()
+                    self.send_rc_command()
                 elif event.type == pygame.QUIT:
-                    should_stop = True
+                    self.DRONE.bye()
+                    sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
+                    if event.key == self.KEY_QUIT:
                         should_stop = True
                     else:
                         self.keydown(event.key)
@@ -100,10 +135,13 @@ class DroneController(object):
             self.screen.fill([0, 0, 0])
 
             frame = frame_read.frame
-            # battery n. 电池
-            text = "Battery: {}%".format(self.tello.get_battery())
-            cv2.putText(frame, text, (5, 720 - 5),
-            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+            # Add drone stats to display
+            text = "Bat: {battery}%, Temp: {temp}°C, Alt: {alt}cm".format(battery=self.DRONE.get_battery(),
+                                                                          temp=self.DRONE.get_temperature(),
+                                                                          alt=self.DRONE.get_altitude())
+            cv2.putText(frame, text, (5, 720 - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             frame = np.rot90(frame)
             frame = np.flipud(frame)
@@ -111,80 +149,63 @@ class DroneController(object):
             frame = pygame.surfarray.make_surface(frame)
             self.screen.blit(frame, (0, 0))
             pygame.display.update()
-
-            time.sleep(1 / FPS)
+            time.sleep(1 / self.DISPLAY_FPS)
 
         # Call it always before finishing. To deallocate resources.
-        # 通常在结束前调用它以释放资源
-        self.tello.end()
+        self.DRONE.bye()
 
     def keydown(self, key):
-        """ Update velocities based on key pressed
+        """ Set RC channel variables from key down, we use standard 4-channel control
         Arguments:
             key: pygame key
-
-        基于键的按下上传各个方向的速度
-        参数：
-            key：pygame事件循环中的键事件
         """
-        if key == pygame.K_UP:  # set forward velocity
-            self.for_back_velocity = S
-        elif key == pygame.K_DOWN:  # set backward velocity
-            self.for_back_velocity = -S
-        elif key == pygame.K_LEFT:  # set left velocity
-            self.left_right_velocity = -S
-        elif key == pygame.K_RIGHT:  # set right velocity
-            self.left_right_velocity = S
-        elif key == pygame.K_w:  # set up velocity
-            self.up_down_velocity = S
-        elif key == pygame.K_s:  # set down velocity
-            self.up_down_velocity = -S
-        elif key == pygame.K_a:  # set yaw counter clockwise velocity
-            self.yaw_velocity = -S
-        elif key == pygame.K_d:  # set yaw clockwise velocity
-            self.yaw_velocity = S
+        if key == self.KEY_FORWARD:
+            self.VELOCITY_FORWARD_BACK = DRONE_SPEED
+        elif key == self.KEY_BACKWARD:
+            self.VELOCITY_FORWARD_BACK = -DRONE_SPEED
+        elif key == self.KEY_LEFT:
+            self.VELOCITY_LEFT_RIGHT = -DRONE_SPEED
+        elif key == self.KEY_RIGHT:
+            self.VELOCITY_LEFT_RIGHT = DRONE_SPEED
+        elif key == self.KEY_UP:
+            self.VELOCITY_UP_DOWN = DRONE_SPEED
+        elif key == self.KEY_DOWN:
+            self.VELOCITY_UP_DOWN = -DRONE_SPEED
+        elif key == self.KEY_ROTATE_LEFT:
+            self.VELOCITY_YAW = -DRONE_SPEED
+        elif key == self.KEY_ROTATE_RIGHT:
+            self.VELOCITY_YAW = DRONE_SPEED
 
     def keyup(self, key):
-        """ Update velocities based on key released
+        """ Set RC channel variables from key release, we use standard 4-channel control
         Arguments:
             key: pygame key
-
-        基于键的松开上传各个方向的速度
-        参数：
-            key：pygame事件循环中的键事件
         """
-        if key == pygame.K_UP or key == pygame.K_DOWN:  # set zero forward/backward velocity
-            self.for_back_velocity = 0
-        elif key == pygame.K_LEFT or key == pygame.K_RIGHT:  # set zero left/right velocity
-            self.left_right_velocity = 0
-        elif key == pygame.K_w or key == pygame.K_s:  # set zero up/down velocity
-            self.up_down_velocity = 0
-        elif key == pygame.K_a or key == pygame.K_d:  # set zero yaw velocity
-            self.yaw_velocity = 0
-        elif key == pygame.K_t:  # takeoff
-            self.tello.takeoff()
-            self.send_rc_control = True
-        elif key == pygame.K_l:  # land
-            not self.tello.land()
-            self.send_rc_control = False
+        if key == self.KEY_TAKEOFF:
+            self.DRONE.takeoff()
+            self.DRONE_SEND_RC_COMMAND = True
+        elif key == self.KEY_LANDING:
+            self.DRONE.land()
+            self.DRONE_SEND_RC_COMMAND = False
+        else:
+            if key == self.KEY_FORWARD or key == self.KEY_BACKWARD:
+                self.VELOCITY_FORWARD_BACK = 0
+            elif key == self.KEY_LEFT or key == self.KEY_RIGHT:
+                self.VELOCITY_LEFT_RIGHT = 0
+            elif key == self.KEY_UP or key == self.KEY_DOWN:
+                self.VELOCITY_UP_DOWN = 0
+            elif key == self.KEY_ROTATE_LEFT or self.KEY_ROTATE_RIGHT:
+                self.VELOCITY_YAW = 0
 
-    def update(self):
-        """ Update routine. Send velocities to Tello.
-
-            向Tello发送各方向速度信息
-        """
-        if self.send_rc_control:
-            self.tello.send_rc_control(self.left_right_velocity, self.for_back_velocity,
-                self.up_down_velocity, self.yaw_velocity)
-
-
-def main():
-    dc = DroneController()
-
-    # run frontend
-
-    dc.run()
+    def send_rc_command(self):
+        """ Send 4-channel rc command """
+        if self.DRONE_SEND_RC_COMMAND is True:
+            self.DRONE.rc_command(self.VELOCITY_LEFT_RIGHT,
+                                  self.VELOCITY_FORWARD_BACK,
+                                  self.VELOCITY_UP_DOWN,
+                                  self.VELOCITY_YAW)
 
 
-if __name__ == '__main__':
-    main()
+dc = DroneController()
+dc.DISPLAY_MODE = [300, 300]
+dc.run()
