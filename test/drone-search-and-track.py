44a59abe8defd619569420a27f989a1a6690a317
Juan Caser <jan_caser@cody.inc>
2023-04-21 12:55:00 +0800

    Initial commit for search and track

---
+++ test/drone-search-and-track.py
@@ -0,0 +1,12 @@
+from lib.drone.ryze_tello import Drone
+import time
+
+drone = Drone()
+drone.hello()
+drone.info()
+# drone.speed(100)
+# drone.takeoff()
+# time.sleep(5)
+# drone.rotate_cw(360)
+# drone.land()
+drone.bye()
